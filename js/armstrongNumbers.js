7807111a45cc4d157b7cf680fce4a36ed795c7d4
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-05-25 09:57:13 -0500

    different approach

--- js/armstrongNumbers.js
+++ js/armstrongNumbers.js
@@ -1,5 +1,21 @@
 // How can you make this more scalable and reusable later?
 
-exports.findArmstrongNumbers = function() {
-
+exports.findArmstrongNumbers = function(start, end) {
+    let result = []	
+    for (let num = start; num <= end; num++){	
+      let factorArray = []	
+      let numString = num.toString();	
+      let numArray = numString.split('');	
+      let length = numArray.length;	
+      numArray.forEach((item) => {	
+        let exponent = item ** length;	
+        factorArray.push(exponent)	
+      });	
+      let sum = factorArray.reduce((a, b) => a + b, 0)	
+      if (sum === num){	
+        result.push(sum);	
+      }	
+    }  	
+    return result;
 };
+console.log(exports.findArmstrongNumbers(0, 999))
