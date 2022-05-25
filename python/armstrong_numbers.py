3811194b240f261c2ecdc1b850da0e8a790acc48
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-05-25 11:32:11 -0500

    better python

--- python/armstrong_numbers.py
+++ python/armstrong_numbers.py
@@ -1,17 +1,15 @@
-# How can you make this more scalable and reusable later?
 def find_armstrong_numbers(start, end):
-    result = []
-    for i in range(start, end):
-        sum = 0
-        num = i
-        digits = str(num)
-        digits = digits.split()
-        for j in range(0, len(digits)):
-            #power = digits[j] ** len(digits)
-            power = pow(int(digits[j]), len(digits))
-            sum += power
-            if sum == num:
-                result.append(num)
-    print(result)
+  result = []
+  for i in range(start, end):
+    sum = 0
+    num = i
+    digits = str(num)
+    digits = [char for char in digits]
+    for j in range(0, len(digits)):
+      power = pow(int(digits[j]), len(digits))
+      sum += power
+      if sum == num:
+        result.append(num)
+  print(result)
 
-print(find_armstrong_numbers(0,999))
+#print(find_armstrong_numbers(0,999))
